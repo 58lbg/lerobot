9c44b2247b450f487ff778eeb19a19d2b5990863
zhangke05 <zhangke05@58.com>
2025-10-21 10:04:58 +0800

    微调数据

--- src/lerobot/model/kinematics.py
+++ src/lerobot/model/kinematics.py
@@ -42,6 +42,7 @@ class RobotKinematics:
 
         self.robot = placo.RobotWrapper(urdf_path)
         self.solver = placo.KinematicsSolver(self.robot)
+        self.solver.enable_velocity_limits(True)
         self.solver.mask_fbase(True)  # Fix the base
 
         self.target_frame_name = target_frame_name
